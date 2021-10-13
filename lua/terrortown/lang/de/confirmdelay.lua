372572a0b89e16fe2f1d7cc8b372dd64ab827848
NickCloud <nicolasbeiglboeck@gmx.at>
2021-10-13 12:31:00 +0200

    Update

--- lua/terrortown/lang/de/confirmdelay.lua
+++ lua/terrortown/lang/de/confirmdelay.lua
@@ -2,7 +2,7 @@ L = LANG.GetLanguageTableReference("de")
 
 L["ttt_cdelay_delaymsg"] = "Du kannst diesen Körper noch nicht untersuchen. Verfügbar in {delay} Sekunden!"
 
-L["submenu_addons_confdelay_title"] = "Untersuchungsverzögerung"
+L["submenu_addons_confdelay_title"] = "Confirm Delay"
 L["header_addons_confdelay"] = "Allgemeine Einstellungen"
 L["help_confdelay_menu"] = "Alle Einstellungen für Confirm Delay von @NickCloudAT"
 
